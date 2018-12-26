53b24bb716b6636f32574506a087e1ee003403a7
Tom Skinner <r.tom.skinner@gmail.com>
2018-12-26 15:39:09 -0800

    Added mLab uri to mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-process.env.MONGODB_URI = 'mongodb://tomskinner:ftarrega50@ds139984.mlab.com:39984/todo-app-api'
 
 mongoose.promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
