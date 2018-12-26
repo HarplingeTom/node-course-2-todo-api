77de0288f587e8549013399b565a0612ed9dda7d
Tom Skinner <r.tom.skinner@gmail.com>
2018-12-26 14:43:30 -0800

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+process.env.MONGODB_URI = 'mongodb://tomskinner:ftarrega50@ds139984.mlab.com:39984/todo-app-api'
 
 mongoose.promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
 
 module.exports = { mongoose };
