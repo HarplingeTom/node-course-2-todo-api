6792a150a3a9139e0ab3e1f12b5dccf348e2d685
Tom Skinner <r.tom.skinner@gmail.com>
2018-12-26 22:07:35 -0800

    Add todos delete by id

--- server/server.js
+++ server/server.js
@@ -46,6 +46,21 @@ app.get('/todos/:id', (req, res) => {
   }).catch(e => res.status(400).send());
 });
 
+app.delete('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(400).send();
+  }
+  Todo.findByIdAndRemove(id).then(todo => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.status(200).send(todo);
+  }).catch(e => {
+    res.status(400).send();
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
