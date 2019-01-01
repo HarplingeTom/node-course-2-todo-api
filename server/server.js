1220556d6722dd7f0818b60fbf5ff5a4a1ae7119
Tom Skinner <r.tom.skinner@gmail.com>
2018-12-31 22:40:50 -0800

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
 const { authenticate } = require('./middleware/authenticate');
+const bcrypt = require('bcryptjs');
 
 const app = express();
 const port = process.env.PORT;
@@ -106,6 +107,17 @@ app.patch('/todos/:id', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+  const body = _.pick(req.body, [ 'email', 'password' ]);
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 app.listen(port, () => {
